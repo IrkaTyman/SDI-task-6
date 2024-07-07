68f6361ae64cfa91a3eda7f6133429ed738cce0f
Irina Tumanova <tymanira@mail.ru>
2024-07-08 00:59:04 +0500

    fix screen tests + configs

--- test/testplane/PageScreenTests.testplane.ts
+++ test/testplane/PageScreenTests.testplane.ts
@@ -5,41 +5,48 @@ describe("PageScreenTests", () => {
     it("Страница Корзины (пустая) отображается без изменений", async ({browser}) => {
         await browser.url(basenameTest+"cart"+bugQueryParams);
 
-        const page = await browser.$('.Cart')
-        await page.assertView('plain', {tolerance: 0.06})
+        const page = await browser.$('.Application')
+        await page.waitForDisplayed()
+        await page.assertView('plain')
     });
 
     it("Страница Каталог отображается без изменений (исключая порядок карточек)", async ({browser}) => {
         await browser.url(basenameTest+"catalog"+bugQueryParams);
 
-        const page = await browser.$('.Catalog')
-        await page.assertView('plain', {tolerance: 0.06})
+        const page = await browser.$('.Application')
+        await page.waitForDisplayed()
+        await page.assertView('plain')
     });
 
     it("Страница Продукт отображается без изменений (исключая данные продукта)", async ({browser}) => {
         await browser.url(basenameTest+"catalog/1"+bugQueryParams);
 
-        const page = await browser.$('.Product')
-        await page.assertView('plain', {tolerance: 0.06})
+        const page = await browser.$('.Application')
+        await page.waitForDisplayed()
+        await page.assertView('plain')
     });
 
     it("Страница Доставка отображается без изменений", async ({browser}) => {
         await browser.url(basenameTest+"delivery"+bugQueryParams);
 
-        const page = await browser.$('.Delivery')
-        await page.assertView('plain', {tolerance: 0.06})
+        const page = await browser.$('.Application')
+        await page.waitForDisplayed()
+        await page.assertView('plain')
     });
 
     it("Страница Главная отображается без изменений", async ({browser}) => {
         await browser.url(basenameTest+bugQueryParams);
 
-        await browser.$('.Home').assertView('plain', {tolerance: 0.06})
+        const page = await browser.$('.Application')
+        await page.waitForDisplayed()
+        await page.assertView('plain')
     });
 
     it("Страница Контакты отображается без изменений", async ({browser}) => {
         await browser.url(basenameTest+"contacts"+bugQueryParams);
 
-        const page = await browser.$('.Contacts')
-        await page.assertView('plain', {tolerance: 0.06})
+        const page = await browser.$('.Application')
+        await page.waitForDisplayed()
+        await page.assertView('plain')
     });
 });
--- test/testplane/bugQueryParams.ts
+++ test/testplane/bugQueryParams.ts
@@ -1,1 +1,1 @@
-export const bugQueryParams = '?bug_id=3'
+export const bugQueryParams = '?bug_id=0'
